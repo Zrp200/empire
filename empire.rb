50480aafeb5fc17b8344f6a5e2d01dafb5e6c226
Zachary Perlmutter <zrp200@gmail.com>
2015-10-05 14:04:16 -0700

    Added a singular version of Army#add_units

--- empire.rb
+++ empire.rb
@@ -1,4 +1,7 @@
+require "singleton"
 class Unit # @abstract
+	include Singleton
+
 	attr_reader :melee_defense, :ranged_defense
 
 	def initialize(melee_defense, ranged_defense)
@@ -27,30 +30,38 @@ end
 class Army
 	attr_reader :stats
 	def initialize
-		@stats = {number: 0, melee_attack: 0, ranged_attack: 0, melee_defense: 0, ranged_defense: 0}
+		@stats = {
+# TODO			looting_capacity: 0,
+			number: 0, 
+			melee_attack: 0, 
+			ranged_attack: 0, 
+			melee_defense: 0, 
+			ranged_defense: 0
+		}
 	end
 
 	def to_h
 		@stats
 	end
-	def add_units(*units)
-		units.each do |unit|
-			raise TypeError, "unit must be a kind of Unit" unless unit.kind_of? Unit
-			@stats.each_key do |attribute|
-				@stats[attribute] += case attribute
-				when :number then 1
-				when :melee_attack
-					if unit.is_a? Melee then unit.melee_attack
-					else 0
-					end
-				when :ranged_attack
-					if unit.is_a? Ranged then unit.ranged_atttack
-					else 0
-					end
-				else
-					unit.send attribute
+	def add_unit(unit)
+		raise TypeError, "unit must be a kind of Unit" unless unit.kind_of? Unit
+		@stats.each_key do |attribute|
+			@stats[attribute] += case attribute
+			when :number then 1
+			when :melee_attack
+				if unit.is_a? Melee then unit.melee_attack
+				else 0
 				end
+			when :ranged_attack
+				if unit.is_a? Ranged then unit.ranged_atttack
+				else 0
+				end
+			else
+				unit.send attribute
 			end
 		end
+	end
+	def add_units(*units)
+		units.each {|unit| add_unit(unit)}
 	end
 end
