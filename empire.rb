67f7a8cbf183a4d39124d98ba9bde908e6e7a519
Zachary Perlmutter <zrp200@gmail.com>
2015-10-05 19:39:32 -0700

    Added Ranged::HeavyCrossbowman

--- empire.rb
+++ empire.rb
@@ -46,6 +46,7 @@ class Ranged < Unit
 	Crossbowman = new 36, 6, 36
 	VeteranCrossbowman = new 98, 16, 26
 	CrossbowmanOfTheKingsguard = new 121, 14, 23
+	HeavyCrossbowman = new 92, 15, 24
 	ScoutOfTheKingsguard = new 16, 64, 139
 	TravelingCrossbowman = new 135, 22, 30
 	Archer = new 10, 53, 55
