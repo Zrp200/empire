9a185e91d36ca94b192d129a01509e53c9b498f1
Zachary Perlmutter <zrp200@gmail.com>
2015-10-19 21:24:18 -0700

    Added more data

--- lib/empire.rb
+++ lib/empire.rb
@@ -1,17 +1,20 @@
 class Unit # @abstract
-	attr_reader :melee_defense, :ranged_defense
+	attr_reader :melee_defense, :ranged_defense, :looting_capacity, :travel_speed, :food_consumption
 
-	def initialize(melee_defense, ranged_defense)
+	def initialize(melee_defense, ranged_defense, looting_capacity, travel_speed, food_consumption)
 		@melee_defense = melee_defense
 		@ranged_defense = ranged_defense
+		@looting_capacity = looting_capacity
+		@travel_speed = travel_speed
+		@food_consumption = food_consumption
 	end
 end
 class Melee < Unit
 	attr_reader :melee_attack
 
-	def initialize(attack, melee_defense, ranged_defense)
+	def initialize(attack, melee_defense, ranged_defense, looting_capacity = 0, travel_speed = 0, food_consumption = 0)
 		@melee_attack =  attack
-		super(melee_defense, ranged_defense)
+		super(melee_defense, ranged_defense, looting_capacity, travel_speed, food_consumption)
 	end
 	def attack_unit?
 		@melee_attack >= @melee_defense
@@ -23,9 +26,9 @@ end
 class Ranged < Unit
 	attr_reader :ranged_attack
 
-	def initialize(attack, melee_defense, ranged_defense)
+	def initialize(attack, melee_defense, ranged_defense, looting_capacity = 0, travel_speed = 0, food_consumption = 0)
 		@ranged_attack = attack
-		super(melee_defense, ranged_defense)
+		super(melee_defense, ranged_defense, looting_capacity, travel_speed, food_consumption)
 	end
 	def attack_unit?
 		@ranged_attack >= @ranged_defense
@@ -38,12 +41,14 @@ class Army
 	attr_reader :stats, :units
 	def initialize
 		@stats = {
-# TODO			looting_capacity: 0,
 			number: 0, 
 			melee_attack: 0, 
 			ranged_attack: 0, 
 			melee_defense: 0, 
-			ranged_defense: 0
+			ranged_defense: 0,
+			looting_capacity: 0,
+			travel_speed: nil,
+			food_consumption: 0
 		}
 		@units = Hash.new(0)
 	end
@@ -54,18 +59,26 @@ class Army
 	def add_unit(unit)
 		raise TypeError, "unit must be a kind of Unit" unless unit.kind_of? Unit
 		@stats.each_key do |attribute|
-			@stats[attribute] += case attribute
-			when :number then 1
-			when :melee_attack
-				if unit.is_a? Melee then unit.melee_attack
-				else 0
-				end
-			when :ranged_attack
-				if unit.is_a? Ranged then unit.ranged_attack
-				else 0
+			unless attribute == :travel_speed
+				@stats[attribute] += case attribute
+				when :number then 1
+				when :melee_attack
+					if unit.is_a? Melee then unit.melee_attack
+					else 0
+					end
+				when :ranged_attack
+					if unit.is_a? Ranged then unit.ranged_attack
+					else 0
+					end
+				else
+					unit.send attribute
 				end
 			else
-				unit.send attribute
+				if @stats[ :travel_speed ].nil?
+					@stats[ :travel_speed ] = (unit.travel_speed == 0) ? nil : unit.travel_speed
+				elsif @stats[:travel_speed] > unit.travel_speed && unit.travel_speed != 0
+					@stats[:travel_speed] = unit.travel_speed
+				end
 			end
 		end
 		@units[unit] += 1
--- lib/units.rb
+++ lib/units.rb
@@ -1,33 +1,59 @@
 require_relative "empire.rb"
-ArmedCitizen = Melee.new 3, 9, 9
-Militia = Melee.new 8, 27, 24
-Spearman = Melee.new 26, 26, 8
-VeteranSpearman = Melee.new 15, 142, 52
-Maceman = Melee.new 38, 38, 6
-VeteranMaceman = Melee.new 118, 20, 6
-Swordsman = Melee.new 61, 5, 3
-VeteranSwordsman = Melee.new 111, 138, 72
-TwoHandedSwordsman = Melee.new 109, 19, 5
+
+ArmedCitizen = Melee.new 3, 9, 9 # Cannot be used in attacks in-game
+Militia = Melee.new 8, 27, 24 # Cannot be used in attacks in-game
+
+Spearman = Melee.new 26, 26, 8, 14, 75, 2
+VeteranSpearman = Melee.new 15, 142, 52, 14, 26, 4
+
+Maceman = Melee.new 38, 38, 6, 32, 73, 2
+VeteranMaceman = Melee.new 118, 20, 6, 32, 22, 4
+
+Swordsman = Melee.new 61, 5, 3, 12, 24, 3
+VeteranSwordsman = Melee.new 111, 138, 72, 29, 28, 6
+
+TwoHandedSwordsman = Melee.new 109, 19, 5, 28, 18, 4
 VeteranTwoHandedSwordsman = Melee.new 125, 20, 6
-Halberdier = Melee.new 17, 135, 45
+
+Halberdier = Melee.new 17, 135, 45, 18, 19, 4
 VeteranHalberdier = Melee.new 15, 145, 55
+
 TravelingKnight = Melee.new 146, 20, 9
-FlameBearer = Melee.new 14, 170, 49
+TravelingCrossbowman = Ranged.new 135, 22, 30
+
+KnightOfTheEliteGuard = Melee.new 140, 18, 5, 33, 28, 4
+CrossbowmanOfTheEliteGuard = Ranged.new 129, 14, 23, 29, 28, 4
+
+# Auxililary Units
+MuscleMan = Melee.new 116, 142, 52, 25
+Marksman = Ranged.new 97, 59, 132, 25, 30
+
+FlameBearer = Melee.new 14, 170, 49, 3, 25, 4
+
 DemonHorror = Melee.new 185, 19, 5
+DeathlyHorror = Ranged.new 162, 15, 24
+
 Lancer = Melee.new 13, 150, 34
+
 Bowman = Ranged.new 24, 8, 24
+
 CompositeBowman = Ranged.new 16, 54, 159
+
 Longbowman = Ranged.new 20, 51, 125
 VeteranLongbowman = Ranged.new 17, 61, 134
+
 Crossbowman = Ranged.new 36, 6, 36
 VeteranCrossbowman = Ranged.new 98, 16, 26
+
 CrossbowmanOfTheKingsguard = Ranged.new 121, 14, 23
+ScoutOfTheKingsGuard = Ranged.new 16, 64, 139
+
 HeavyCrossbowman = Ranged.new 92, 15, 24
 VeteranHeavyCrossbowman = Ranged.new 114, 16, 26
-ScoutOfTheKingsguard = Ranged.new 16, 64, 139
-TravelingCrossbowman = Ranged.new 135, 22, 30
+
 Archer = Ranged.new 10, 53, 55
+
 SpearThrower = Ranged.new 14, 35, 139
 RenegadeSpearThrower = Ranged.new 14, 20, 139
-DeathlyHorror = Ranged.new 162, 15, 24
+
 CultistBowman = Ranged.new 92, 51, 125
